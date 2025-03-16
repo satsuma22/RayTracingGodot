1e754321d429bc74c17368901ed4d7d7ba60df5a
shalin_ <shalindahiya@gmail.com>
2025-03-16 12:18:33 +0100

    add a sphere and perform basic ray tracing

--- compute_shader.glsl
+++ compute_shader.glsl
@@ -5,8 +5,65 @@ layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
 layout(rgba8, binding = 0) uniform image2D output_texture;
 
+struct Camera {
+    vec3 position;
+    mat3 basis;
+    float fov;
+    float aspect;
+};
+
+struct Sphere {
+    vec3 center;
+    float radius;
+};
+
+layout(set = 0, binding = 1, std430) restrict buffer Uniforms {
+    Camera camera;
+    Sphere sphere;
+};
+
+bool raySphereIntersect(vec3 origin, vec3 dir, vec3 center, float radius)
+{
+    vec3 oc = origin - center;
+    float a = dot(dir, dir);
+    float b = 2.0 * dot(oc, dir);
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - 4 * a * c;
+
+    if (discriminant <= 0.0)
+    {
+        return false;
+    }
+
+    return true;
+}
+
 void main() {
     ivec2 coords = ivec2(gl_GlobalInvocationID.xy);
-    vec4 color = vec4(float(coords.x) / 512.0, float(coords.y) / 512.0, 0.5, 1.0);
+    ivec2 imageSize = imageSize(output_texture);
+
+    if (coords.x >= imageSize.x || coords.y >= imageSize.y)
+    {
+        return;
+    }
+
+    float u = (float(coords.x) + 0.5) / float(imageSize.x);
+    float v = (float(coords.y) + 0.5) / float(imageSize.y);
+
+    float tanFov = tan(radians(camera.fov) / 2.0);
+    float x = (-camera.aspect * tanFov) + (2.0 * camera.aspect * tanFov * u);
+    float y = tanFov - (2.0 * tanFov * v);
+    vec3 dirCamera = vec3(x, y, -1.0);
+
+    vec3 dirWorld = camera.basis * dirCamera;
+    vec3 origin = camera.position;
+
+    vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
+
+    if( raySphereIntersect(origin, dirWorld, sphere.center, sphere.radius) )
+    {
+        color = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+    
     imageStore(output_texture, coords, color);
 }
--- ray_tracer.gd
+++ ray_tracer.gd
@@ -4,10 +4,16 @@ var image: Image
 var texture: ImageTexture
 var rd: RenderingDevice
 var texture_rid: RID
+var storage_buffer_rid: RID
 var shader: RID
 var pipeline: RID
 var uniform_set: RID
 
+var width: int = 512
+var height: int = 512
+
+@onready var camera = $Camera3D
+@onready var sphere = $MeshInstance3D
 @onready var texture_rect = $CanvasLayer/TextureRect
 
 # Called when the node enters the scene tree for the first time.
@@ -18,6 +24,7 @@ func _ready() -> void:
 	image.fill(Color(0,0,0,1))
 	
 	texture_rid = create_compute_texture()
+	storage_buffer_rid = create_compute_storage()
 	
 	texture = ImageTexture.create_from_image(image)
 	texture_rect.texture = texture
@@ -32,10 +39,48 @@ func create_compute_texture() -> RID:
 	tex_format.format = RenderingDevice.DATA_FORMAT_R8G8B8A8_UNORM
 	tex_format.usage_bits = RenderingDevice.TEXTURE_USAGE_STORAGE_BIT | RenderingDevice.TEXTURE_USAGE_CAN_UPDATE_BIT | RenderingDevice.TEXTURE_USAGE_CAN_COPY_FROM_BIT
 	
-	var img_data = image.get_data()
+	#var img_data = image.get_data()
 	var tex_view = RDTextureView.new()
 	
 	return rd.texture_create(tex_format, tex_view)
+	
+func create_compute_storage() -> RID:
+	var buffer := PackedByteArray()
+	
+	buffer.append_array(_vector3_to_bytes(camera.global_transform.origin))
+	buffer.append_array(_float_to_bytes(0.0))
+	buffer.append_array(_vector3_to_bytes(camera.basis.x))
+	buffer.append_array(_float_to_bytes(0.0))
+	buffer.append_array(_vector3_to_bytes(camera.basis.y))
+	buffer.append_array(_float_to_bytes(0.0))
+	buffer.append_array(_vector3_to_bytes(camera.basis.z))
+	buffer.append_array(_float_to_bytes(0.0))
+	buffer.append_array(_float_to_bytes(camera.fov))
+	buffer.append_array(_float_to_bytes(float(width)/ height))
+	
+	buffer.append_array(_float_to_bytes(0.0))
+	buffer.append_array(_float_to_bytes(0.0))
+	
+	buffer.append_array(_vector3_to_bytes(sphere.global_transform.origin))
+	buffer.append_array(_float_to_bytes(sphere.global_transform.basis.get_scale().length()))
+	
+	'''
+	buffer.append_array(_vector3_to_bytes(Vector3(0.0, 0.0, 0.0)))  # Camera at origin
+	buffer.append_array(_float_to_bytes(0.0))
+	buffer.append_array(_vector3_to_bytes(Vector3(1.0, 0.0, 0.0)))   # Basis X
+	buffer.append_array(_float_to_bytes(0.0))
+	buffer.append_array(_vector3_to_bytes(Vector3(0.0, 1.0, 0.0)))   # Basis Y
+	buffer.append_array(_float_to_bytes(0.0))
+	buffer.append_array(_vector3_to_bytes(Vector3(0.0, 0.0, 1.0)))   # Basis Z
+	buffer.append_array(_float_to_bytes(0.0))
+	buffer.append_array(_float_to_bytes(90.0))                       # FOV
+	buffer.append_array(_float_to_bytes(1.0))                        # Aspect
+	buffer.append_array(_float_to_bytes(0.0))
+	buffer.append_array(_float_to_bytes(0.0))
+	buffer.append_array(_vector3_to_bytes(Vector3(0.0, 0.0, -3.0)))  # Sphere center
+	buffer.append_array(_float_to_bytes(1.0))                        # Sphere radius
+	'''
+	return rd.storage_buffer_create(buffer.size(), buffer)
 	
 func setup_compute_shader():
 	var shader_file = load("res://compute_shader.glsl")
@@ -43,11 +88,17 @@ func setup_compute_shader():
 	shader = rd.shader_create_from_spirv(shader_spirv)
 	pipeline = rd.compute_pipeline_create(shader)
 	
-	var uniform = RDUniform.new()
-	uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
-	uniform.binding = 0
-	uniform.add_id(texture_rid)
-	uniform_set = rd.uniform_set_create([uniform], shader, 0)
+	var texture_uniform = RDUniform.new()
+	texture_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
+	texture_uniform.binding = 0
+	texture_uniform.add_id(texture_rid)
+	
+	var buffer_uniform := RDUniform.new()
+	buffer_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	buffer_uniform.binding = 1
+	buffer_uniform.add_id(storage_buffer_rid)
+	
+	uniform_set = rd.uniform_set_create([texture_uniform, buffer_uniform], shader, 0)
 
 func run_compute_shader():
 	var compute_list = rd.compute_list_begin()
@@ -64,6 +115,16 @@ func update_texture():
 	var tex_data = rd.texture_get_data(texture_rid, 0)
 	image = Image.create_from_data(512, 512, false, Image.FORMAT_RGBA8, tex_data)
 	texture.update(image)
+	
+func _vector3_to_bytes(v: Vector3) -> PackedByteArray:
+	var bytes := PackedByteArray()
+	bytes.append_array(PackedFloat32Array([v.x]).to_byte_array())
+	bytes.append_array(PackedFloat32Array([v.y]).to_byte_array())
+	bytes.append_array(PackedFloat32Array([v.z]).to_byte_array())
+	return bytes
+	
+func _float_to_bytes(f: float) -> PackedByteArray:
+	return PackedFloat32Array([f]).to_byte_array()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
